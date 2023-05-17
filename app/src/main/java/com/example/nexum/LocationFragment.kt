36eba7c3660c3245800cb31598f057e68b4a68ba
Harshit <reddyharshit8@gmail.com>
2023-05-17 14:23:00 +0530

    location thingy

--- app/src/main/java/com/example/nexum/LocationFragment.kt
+++ app/src/main/java/com/example/nexum/LocationFragment.kt
@@ -1,10 +1,29 @@
 package com.example.nexum
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Criteria
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.core.app.ActivityCompat
+import androidx.core.location.LocationManagerCompat.getCurrentLocation
+import com.google.android.gms.maps.CameraUpdateFactory
+//import androidx.lifecycle.viewmodel.CreationExtras.Empty.map
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.LatLng
+
+
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
@@ -20,19 +39,124 @@ class LocationFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
+
+    // Request code for location permission
+        private val LOCATION_PERMISSION_REQUEST_CODE = 1
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+
+            // Request location permission
+            if (ActivityCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    LOCATION_PERMISSION_REQUEST_CODE
+                )
+            }
         }
-    }
+
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            super.onViewCreated(view, savedInstanceState)
+
+            val mapFragment =
+                childFragmentManager.findFragmentById(R.id.map_fragment) as? SupportMapFragment
+            mapFragment?.getMapAsync { googleMap ->
+                    getCurrentLocation(googleMap)
+                val btnMyLocation = view.findViewById<Button>(R.id.btn_my_location)
+                btnMyLocation.setOnClickListener {
+
+                }
+            }
+        }
+
+        private fun getCurrentLocation(googleMap: GoogleMap?) {
+            val locationManager =
+                activity?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            val criteria = Criteria()
+            val provider = locationManager.getBestProvider(criteria, false)
+
+            if (provider != null) {
+                if (ActivityCompat.checkSelfPermission(
+                        requireContext(),
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        requireContext(),
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    return
+                }
+                locationManager.requestLocationUpdates(
+                    provider,
+                    1000,
+                    0F,
+                    object : LocationListener {
+                        override fun onLocationChanged(location: Location) {
+                            // Remove previous marker
+                            googleMap?.clear()
+
+                            // Add new marker to current location
+                            val currentLatLng = LatLng(location.latitude, location.longitude)
+                            googleMap?.addMarker(MarkerOptions().position(currentLatLng))
+
+                            // Center camera on current location
+                            // Create a CameraPosition object for the current location
+                            val cameraPosition = CameraPosition.Builder()
+                                .target(currentLatLng)
+                                .zoom(16F)
+                                .tilt(30F)
+                                .bearing(0F)
+                                .build()
+
+// Create a CameraUpdate object from the CameraPosition object
+                            val cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition)
+
+// Apply the CameraUpdate to the map
+                            googleMap?.animateCamera(cameraUpdate)
+
+
+                            // Stop listening for location updates
+                            locationManager.removeUpdates(this)
+                        }
+
+                        override fun onProviderDisabled(provider: String) {}
+                        override fun onProviderEnabled(provider: String) {}
+                        override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
+                    })
+            }
+        }
+
+
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
+
+
         return inflater.inflate(R.layout.fragment_location, container, false)
     }
+    //17.572166866434433, 78.43342470827403
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//
+//        val mapFragment = childFragmentManager.findFragmentById(R.id.map_fragment) as? SupportMapFragment
+//        mapFragment?.getMapAsync { googleMap ->
+//            getCurrentLocation(googleMap)
+//
+//            // Find the button and set an OnClickListener to it
+//            val btnMyLocation = view.findViewById<Button>(R.id.btn_my_location)
+//            btnMyLocation.setOnClickListener {
+//                getCurrentLocation(googleMap)
+//            }
+//        }
+//    }
 
     companion object {
         /**
---
+++ app/src/main/java/com/example/nexum/model/heatmapData.kt
@@ -0,0 +1,7 @@
+package com.example.nexum.model
+
+data class heatmapData(
+    val lat: Double,
+    val lng: Double,
+    val intensity: Double
+)
