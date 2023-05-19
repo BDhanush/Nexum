66ad575540a6c49ca6e7a957ed61d26dfda68921
Dhanush <dhanushbommavaram@gmail.com>
2023-05-19 16:35:35 +0530

    location heatmap

--- app/src/main/java/com/example/nexum/LocationFragment.kt
+++ app/src/main/java/com/example/nexum/LocationFragment.kt
@@ -15,7 +15,6 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.viewmodel.CreationExtras.Empty.map
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
