daa9e6d62ab0fc963fb5130da55d28bf78ff148b
Suchir <spsuchir@outlook.com>
2023-05-20 19:56:57 +0530

    Integrated sign up and login front end and back end

---
+++ app/src/main/java/com/example/nexum/SignupActivity.kt
@@ -0,0 +1,79 @@
+package com.example.nexum
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.nexum.model.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
+
+class SignupActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+        val signupButton:Button=findViewById(R.id.signupButton)
+        val loginButton:Button=findViewById(R.id.loginButton)
+
+        loginButton.setOnClickListener {
+            finish()
+            Intent(this,LoginActivity::class.java).also{
+                startActivity(it)
+            }
+        }
+        signupButton.setOnClickListener {
+            signup()
+        }
+    }
+    private fun signup()
+    {
+        val email:EditText=findViewById(R.id.emailInput)
+        val password:EditText=findViewById(R.id.passwordInput)
+        val confirmPassword:EditText=findViewById(R.id.confirmPasswordInput)
+        val username:EditText=findViewById(R.id.usernameInput)
+
+        auth = Firebase.auth
+        database = FirebaseDatabase.getInstance("https://shortvideoapp-e7456-default-rtdb.asia-southeast1.firebasedatabase.app/").reference
+
+        if (password.text.toString() != confirmPassword.text.toString()) {
+            Toast.makeText(
+                this, "Passwords do not match.",
+                Toast.LENGTH_SHORT
+            ).show()
+            return
+        }
+
+        auth.createUserWithEmailAndPassword(email.text.toString(),password.text.toString()).addOnCompleteListener(this) {
+
+            if (it.isSuccessful) {
+                Toast.makeText(
+                    this, "Authentication successful.",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                writeNewUser(auth.currentUser!!.uid, firstName = "", lastName = "", email = email.text.toString())
+
+                finish()
+
+            } else {
+                Toast.makeText(
+                    this, "Authentication failed.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+    fun writeNewUser(uid:String,firstName:String,lastName:String,email:String) {
+        val user = User(firstName,lastName,email);
+        database.child("users").child(uid).setValue(user)
+    }
+}
+
