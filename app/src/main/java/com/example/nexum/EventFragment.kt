790c4487a2f2d66915eb18ec2a190df8a10ee239
Dhanush <dhanushbommavaram@gmail.com>
2023-05-27 20:48:42 +0530

    merge

--- app/src/main/java/com/example/nexum/EventFragment.kt
+++ app/src/main/java/com/example/nexum/EventFragment.kt
@@ -72,8 +72,6 @@ class EventFragment : Fragment() {
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
         }
-        progressBar= requireView().findViewById(R.id.progressBar)
-        progressBar.show()
         readEvents(param1!!.toInt())
 
     }
@@ -83,6 +81,7 @@ class EventFragment : Fragment() {
 
         super.onViewCreated(view, savedInstanceState)
 
+        progressBar= requireView().findViewById(R.id.progressBar)
         eventRecyclerView=view.findViewById(R.id.eventRecyclerView)
         val linearLayoutManager= LinearLayoutManager(this.context)
         eventRecyclerView.layoutManager=linearLayoutManager
